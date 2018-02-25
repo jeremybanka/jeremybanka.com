fd4b6ae39b138facea9140dce66ec4df1d721b38
Jeremy Banka <jeremybanka@gmail.com>
2018-02-25 02:33:08 -0800

    Big TOC Update, Manual Test

--- scripts/table-of-contents.js
+++ scripts/table-of-contents.js
@@ -1,4 +1,4 @@
-/* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
+/* Toggle add-remove the "responsive" class to the table of contents when the user clicks on the icon */
 function mobileContents() {
     var x = document.getElementById("contents");
     if (x.className === "tocbar") {
@@ -8,6 +8,7 @@ function mobileContents() {
     }
 }
 
+/* Smoothscroll charm */
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
@@ -15,3 +16,5 @@ $(document).on('click', 'a[href^="#"]', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+
+/* Smoothscroll charm */
