c1e3e2d30fbc2d44594f5d1b7b3c7d8a009e6115
Jeremy Banka <jeremybanka@gmail.com>
2018-11-02 18:37:30 -0700

    Mega-Update
    • Finished Overhaul
    • Added Sudo
    • Added Display Type
    • Added _graphics folder
    • "graphic" spreads fix squishy height bug with zoom.js

--- scripts/table-of-contents.js
+++ scripts/table-of-contents.js
@@ -1,20 +1,23 @@
 /* Toggle add-remove the "responsive" class to the table of contents when the user clicks on the icon */
 function mobileContents() {
-    var x = document.getElementById("contents");
-    if (x.className === "tocbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "tocbar";
-    }
+  var x = document.getElementById("contents");
+  if (x.className === "tocbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "tocbar";
+  }
 }
 
 /* Smoothscroll charm */
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
+$(document).on("click", 'a[href^="#"]', function(event) {
+  event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    500
+  );
 });
 
 /* Smoothscroll charm */
