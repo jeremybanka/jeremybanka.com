41f23a6b9729cdc72da60af6002c46d04e5ac3eb
Jeremy Banka <jeremybanka@gmail.com>
2018-11-11 17:01:11 -0800

    Small Bugfixes

--- scripts/table-of-contents.js
+++ scripts/table-of-contents.js
@@ -8,6 +8,34 @@ function mobileContents() {
   }
 }
 
+/* get 
+var accordions = document.getElementsByClassName("accordion");
+var closeElements = document.getElementsByClassName("close");
+var i;
+
+for (i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var sub = this.nextElementSibling;
+    if (sub.style.maxHeight) {
+      sub.style.maxHeight = null;
+    } else {
+      sub.style.maxHeight = sub.scrollHeight + "px";
+    }
+  });
+}
+for (i = 0; i < closeElements.length; i++) {
+  closeElements[i].addEventListener("click", function() {
+    var sect = this.parentElement;
+    sect.previousElementSibling.classList.toggle("active");
+    if (sect.style.maxHeight) {
+      sect.style.maxHeight = null;
+    } else {
+      sect.style.maxHeight = auto;
+    }
+  });
+} */
+
 /* Smoothscroll charm */
 $(document).on("click", 'a[href^="#"]', function(event) {
   event.preventDefault();
