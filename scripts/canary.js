ddebf700cb80bc651611f7709009f6fe5dfaa7b1
Jeremy Banka <jeremybanka@gmail.com>
2018-11-11 23:02:44 -0800

    Theme Update
    Fix Favicons

--- scripts/canary.js
+++ scripts/canary.js
@@ -1,25 +1,13 @@
 let lightswitch = document.getElementById("lightswitch");
 let theme = document.getElementById("theme");
 
-function triggerAnimation() {
-  lightswitch.classList.remove("run-animation");
-  void lightswitch.offsetWidth;
-  lightswitch.classList.add("run-animation");
-}
-
 /* eslint-disable no-unused-vars */
 
 function flipSwitch() {
-  if (lightswitch.classList.contains("dark")) {
-    theme.setAttribute("href", "/styles/themes/graphite.css");
-    lightswitch.classList.remove("dark");
-    console.log("enable graphite"); // eslint-disable-line
-  } else {
-    theme.setAttribute("href", "/styles/themes/twilight.css");
-    lightswitch.classList.add("dark");
-    console.log("enable twilight"); // eslint-disable-line
-  }
-  triggerAnimation();
+  const isDark = lightswitch.classList.contains("dark");
+  const newTheme = isDark ? "graphite" : "charcoal";
+  theme.setAttribute("href", `/styles/themes/${newTheme}.css`);
+  lightswitch.classList.toggle("dark");
 }
 
 /* eslint-enable no-unused-vars */
