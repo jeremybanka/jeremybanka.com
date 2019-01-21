c825a698c447d7e4e65e1d427db1bd4eac2ac90a
Jeremy Banka <jeremybanka@gmail.com>
2019-01-20 17:30:04 -0800

    Update image-strips.js
    for loop becomes foreach method

--- scripts/image-strip.js
+++ scripts/image-strip.js
@@ -1,34 +1,35 @@
 const w_GridGap = 10;
-let g_ImageStrips = document.getElementsByClassName("imagestrip");
-// let w_ViewStrip = g_ImageStrips[0].offsetWidth;
+let g_Imagestrips = document.getElementsByClassName("imagestrip");
+// let w_ViewStrip = g_Imagestrips[0].offsetWidth;
 
 function buildStrip() {
-  for (i = 0; i < g_ImageStrips.length; i++) {
-    // for each imagestrip element
-    let g_Images = g_ImageStrips[i].children;
-    let n_Images = g_Images.length;
-    let wT_GridGaps = w_GridGap * (n_Images + 1);
+  for (i = 0; i < g_Imagestrips.length; i++) {
+    // for each Imagestrip element
+    let g_Image = g_Imagestrips[i].children;
+    let a_Image = Array.from(g_Image);
+    let n_Image = g_Image.length;
+    let wT_GridGaps = w_GridGap * (n_Image + 1);
     // let w_Available = w_ViewStrip - wT_GridGaps;
 
-    let rT_ConstrainedImages = 0;
-    for (j = 0; j < g_Images.length; j++) {
-      /* find width of total composite images */
-      let r_Landscape = g_Images[j].naturalWidth / g_Images[j].naturalHeight;
-      rT_ConstrainedImages += r_Landscape;
-    }
-    for (k = 0; k < g_Images.length; k++) {
+    let rT_ConstrainedImage = 0;
+    a_Image.forEach(e_Image => {
+      let r_Landscape = e_Image.naturalWidth / e_Image.naturalHeight;
+      rT_ConstrainedImage += r_Landscape;
+    });
+
+    for (k = 0; k < g_Image.length; k++) {
       /* for each image */
-      let r_Landscape = g_Images[k].naturalWidth / g_Images[k].naturalHeight;
-      let rP_Image_ConstrainedImages = r_Landscape / rT_ConstrainedImages;
+      let r_Landscape = g_Image[k].naturalWidth / g_Image[k].naturalHeight;
+      let rP_Image_ConstrainedImage = r_Landscape / rT_ConstrainedImage;
 
-      g_Images[k].style.setProperty("--rP-I-CI", rP_Image_ConstrainedImages);
-      g_Images[k].style.setProperty("--wT-GG", wT_GridGaps + "px");
+      g_Image[k].style.setProperty("--rP-I-CI", rP_Image_ConstrainedImage);
+      g_Image[k].style.setProperty("--wT-GG", wT_GridGaps + "px");
 
-      console.log(rP_Image_ConstrainedImages); /* interrogator */
+      console.log(rP_Image_ConstrainedImage); /* interrogator */
 
       /*
-      let w_Image = rP_Image_ConstrainedImages * w_Available; 
-      g_Images[k].style.width = w_Image + "px"; 
+      let w_Image = rP_Image_ConstrainedImage * w_Available; 
+      g_Image[k].style.width = w_Image + "px"; 
       
       :^: Crude function that changes the width of each image to exactly what it should be upon load.
       :ˇ: Below is a sophisticated version that uses CSS calc. 
