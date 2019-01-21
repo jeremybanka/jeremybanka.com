0b650ebfdb6df15998b76b683064204db8cf1394
Jeremy Banka <jeremybanka@gmail.com>
2019-01-20 17:54:42 -0800

    Simplify another nested for loop

--- scripts/image-strip.js
+++ scripts/image-strip.js
@@ -5,37 +5,22 @@ let g_Imagestrips = document.getElementsByClassName("imagestrip");
 function buildStrip() {
   for (i = 0; i < g_Imagestrips.length; i++) {
     // for each Imagestrip element
-    let g_Image = g_Imagestrips[i].children;
-    let a_Image = Array.from(g_Image);
-    let n_Image = g_Image.length;
-    let wT_GridGaps = w_GridGap * (n_Image + 1);
-    // let w_Available = w_ViewStrip - wT_GridGaps;
+    const g_Image = g_Imagestrips[i].children;
+    const a_Image = Array.from(g_Image);
+    const n_Image = g_Image.length;
+    const wT_GridGaps = w_GridGap * (n_Image + 1);
 
-    let rT_ConstrainedImage = 0;
+    const rT_ConstrainedImage = a_Image.reduce(
+      (memo, e_Image) => memo + e_Image.naturalWidth / e_Image.naturalHeight,
+      0
+    );
+
     a_Image.forEach(e_Image => {
-      let r_Landscape = e_Image.naturalWidth / e_Image.naturalHeight;
-      rT_ConstrainedImage += r_Landscape;
+      const r_Landscape = e_Image.naturalWidth / e_Image.naturalHeight;
+      const rP_Image_ConstrainedImage = r_Landscape / rT_ConstrainedImage;
+      e_Image.style.setProperty("--rP-I-CI", rP_Image_ConstrainedImage);
+      e_Image.style.setProperty("--wT-GG", wT_GridGaps + "px");
     });
-
-    for (k = 0; k < g_Image.length; k++) {
-      /* for each image */
-      let r_Landscape = g_Image[k].naturalWidth / g_Image[k].naturalHeight;
-      let rP_Image_ConstrainedImage = r_Landscape / rT_ConstrainedImage;
-
-      g_Image[k].style.setProperty("--rP-I-CI", rP_Image_ConstrainedImage);
-      g_Image[k].style.setProperty("--wT-GG", wT_GridGaps + "px");
-
-      console.log(rP_Image_ConstrainedImage); /* interrogator */
-
-      /*
-      let w_Image = rP_Image_ConstrainedImage * w_Available; 
-      g_Image[k].style.width = w_Image + "px"; 
-      
-      :^: Crude function that changes the width of each image to exactly what it should be upon load.
-      :ˇ: Below is a sophisticated version that uses CSS calc. 
-      :ˇ: It modifies variables listed in each image's style tag
-      */
-    }
   }
 }
 
