655504c2a6a3b5045c131322dbcdd92370c81d47
Jeremy Banka <jeremybanka@gmail.com>
2019-01-20 19:04:49 -0800

    Replace the last for loop
    only one global constant, GRIDGAP
    array immediatiely builds from getElementsByClassName without an intermediate variable the stores an HTML collection

--- scripts/image-strip.js
+++ scripts/image-strip.js
@@ -1,27 +1,24 @@
-const w_GridGap = 10;
-let g_Imagestrips = document.getElementsByClassName("imagestrip");
-// let w_ViewStrip = g_Imagestrips[0].offsetWidth;
+const W_GRIDGAP = 10;
 
 function buildStrip() {
-  for (i = 0; i < g_Imagestrips.length; i++) {
-    // for each Imagestrip element
-    const g_Image = g_Imagestrips[i].children;
-    const a_Image = Array.from(g_Image);
-    const n_Image = g_Image.length;
-    const wT_GridGaps = w_GridGap * (n_Image + 1);
-
+  const a_Imagestrip = Array.from(
+    document.getElementsByClassName("imagestrip")
+  );
+  a_Imagestrip.forEach(e_Imagestrip => {
+    const a_Image = Array.from(e_Imagestrip.children);
     const rT_ConstrainedImage = a_Image.reduce(
       (memo, e_Image) => memo + e_Image.naturalWidth / e_Image.naturalHeight,
       0
     );
 
+    const wT_GridGap = W_GRIDGAP * (a_Image.length + 1);
     a_Image.forEach(e_Image => {
       const r_Landscape = e_Image.naturalWidth / e_Image.naturalHeight;
       const rP_Image_ConstrainedImage = r_Landscape / rT_ConstrainedImage;
       e_Image.style.setProperty("--rP-I-CI", rP_Image_ConstrainedImage);
-      e_Image.style.setProperty("--wT-GG", wT_GridGaps + "px");
+      e_Image.style.setProperty("--wT-GG", wT_GridGap + "px");
     });
-  }
+  });
 }
 
 window.onload = function() {
